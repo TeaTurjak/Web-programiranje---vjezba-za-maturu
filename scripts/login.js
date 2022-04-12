b3cb2ffc13bc785f0dcdb493b00da01ec63338a6
mihaelamandic <mihaela.mandic2829@gmail.com>
2022-04-12 16:58:20 +0200

    Creating javascript file for Login page

---
+++ scripts/login.js
@@ -0,0 +1,39 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if ( username == "Tea" && password == "test123"){
+        alert ("Login successfully");
+        window.location = "homepage.html"; // Redirecting to other page.
+        return false;
+    }
+    else{
+        attempt --;// Decrementing by one.
+        alert("You have left "+attempt+" attempt;");
+        // Disabling fields after 3 attempts.
+        if( attempt == 0){
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("login-form-submit").disabled = true;
+            return false;
+        }
+    }
+}
+
+function validateEmailForm(){
+    var name=document.getElementById("username").value;
+    if(name == null || name == ""){
+        document.getElementById("error-Username").innerHTML="E-mail cannot be empty";
+      
+    }
+   
+}
+
+function validatePasswordForm(){
+    var password = document.getElementById("password").value;
+    if(password == null || password == ""){
+        document.getElementById("error-Password").innerHTML="Password cannot be empty";
+    }
+}
+
